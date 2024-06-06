597ec202615d26f37b1c4b52f7452319eba62920
Takeshi Ooka <74577802+t-ooka@users.noreply.github.com>
2024-06-06 23:26:47 +0900

    Update step3.py

--- Longest Consectuive Sequence/step3.py
+++ Longest Consectuive Sequence/step3.py
@@ -2,7 +2,7 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         num_set = set(nums)
 
-        longest_consecutive_sequence = 1
+        longest_consecutive_sequence = 0
 
         for num in nums:
             if num - 1 not in num_set:
