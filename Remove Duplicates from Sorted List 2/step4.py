270400e4ff353af8ea5cbe5bddd3170e8853b3e5
Takeshi Ooka <74577802+t-ooka@users.noreply.github.com>
2025-09-02 23:14:29 +0900

    Create step4.py

---
+++ Remove Duplicates from Sorted List 2/step4.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+INITIAL_VALUE = -1000
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(INITIAL_VALUE, head)
+        previous_node, current_node = dummy, head
+
+        while current_node:
+            while current_node.next and current_node.val == current_node.next.val:
+                current_node = current_node.next
+            if previous_node.next == current_node:
+                previous_node = previous_node.next
+            else:
+                previous_node.next = current_node.next
+              
+            current_node = current_node.next 
+      
+        return dummy.next
+        
