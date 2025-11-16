c8389169b6f61a8545d834818d1e476f82f1f728
Takeshi Ooka <74577802+t-ooka@users.noreply.github.com>
2025-11-16 22:41:15 +0900

    Implement topKFrequent using a heap

---
+++ Top K Frequent Elements (retry)/step3-using-heap.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counts = {}
+        for num in nums:
+            counts[num] = 1 + counts.get(num, 0)
+        heap = []
+        for num, freq in counts.items():
+            heapq.heappush(heap, (freq, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        result = []
+        while heap:
+            result.append(heapq.heappop(heap)[1])
+        return result
